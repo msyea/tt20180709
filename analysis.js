e1b8df5c2abda37384f58a35c5f32a32a8977e80
Simon Mayes <simon@msyea.co.uk>
2018-07-09 12:01:21 +0100

    Adding finding primes implementation

--- analysis.js
+++ analysis.js
@@ -1,3 +1,6 @@
+
+const { listPrimes } = require('./utils')
+
 const tokenize = (input) => {
   // remove apostrophes and hyphens as they're part of the word
   let output = input.replace(/['-]/g, '')
@@ -7,11 +10,11 @@ const tokenize = (input) => {
   return output.trim().toLowerCase().split(' ')
 }
 
-
 exports.concordance = (input) => {
   const count = {}
   const list = []
   const words = tokenize(input)
+  let maxCount = 0
   words.forEach((word) => {
     if (list.indexOf(word) === -1) {
       count[word] = 1
@@ -20,11 +23,23 @@ exports.concordance = (input) => {
       // eslint-disable-next-line no-plusplus
       count[word]++
     }
+    maxCount = count[word] > maxCount ? count[word] : maxCount
   })
   // sort the list
   list.sort()
+  // list the min number of primes as finding them is expensive
+  const primesNumbers = listPrimes(maxCount)
+  const primes = []
+  list.forEach((word) => {
+    // if count of word is a prime, add to primes
+    if (primesNumbers.indexOf(count[word]) !== -1) {
+      primes.push(word)
+    }
+  })
+
   return {
     count,
-    list
+    list,
+    primes
   }
 }

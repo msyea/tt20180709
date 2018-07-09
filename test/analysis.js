7fa7d8cf3e991ed54344bfe7910bd4a69f36154d
Simon Mayes <simon@msyea.co.uk>
2018-07-09 11:17:45 +0100

    Add failing test for listing words

--- test/analysis.js
+++ test/analysis.js
@@ -1,6 +1,23 @@
 
+require('should')
+
+const { concordance } = require('../analysis')
+
+const input = `They were not railway children to begin with. I don't suppose they had
+ever thought about railways except as a means of getting to Maskelyne
+and Cook's, the Pantomime, Zoological Gardens, and Madame Tussaud's.`
+
 describe('text analysis', () => {
-  it('should output the individual words that appear in the book')
+  it('should output the individual words that appear in the book', () => {
+    const { list } = concordance(input)
+    list.should.be.Array()
+    list.should.containEql('they')
+    list.should.containEql('were')
+    list.should.containEql('not')
+    list.should.containEql('railway')
+    list.should.containEql('zoological')
+    list.should.containEql('gardens')
+  })
   it('should list how many times that a word appears')
   it('should identify if the occurances of a word is a prime number')
 })

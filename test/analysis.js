3b80b687a07374b9772ad074c466ff106923ab4a
Simon Mayes <simon@msyea.co.uk>
2018-07-09 10:35:02 +0100

    Writing initial tests from sepc

---
+++ test/analysis.js
@@ -0,0 +1,6 @@
+
+describe('text analysis', () => {
+  it('should output the individual words that appear in the book')
+  it('should list how many times that a word appears')
+  it('should identify if the occurances of a word is a prime number')
+})
